39250ab3e1be784ef66ae15cdec4f82392bb575e
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:05:13 +0530

    Validate uniqueness of friend_requests.recipient_id

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -5,6 +5,8 @@ class FriendRequest < ApplicationRecord
 
   # Validations
 
+  validates :recipient_id, :uniqueness => { :scope => [:sender_id], :message => "already requested" }
+
   validates :recipient_id, :presence => true
 
   validates :sender_id, :presence => true
