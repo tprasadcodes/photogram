dfed6fa38d8f6dde4f4bfd2bf131a3d5c47eef07
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:08:04 +0530

    Direct association between UserResource and CommentResource

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -12,6 +12,9 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :comments,
+             foreign_key: :commenter_id
+
   has_many   :likes,
              resource: VoteResource
 
