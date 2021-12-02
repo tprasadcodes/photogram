5c6af9e3d0e02b0e0bd62ef843dcacdaccf29a6e
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:05:12 +0530

    Validate presence of votes.user_id

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,8 @@ class Vote < ApplicationRecord
 
   # Validations
 
+  validates :user_id, :presence => true
+
   # Scopes
 
   def to_s
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe Vote, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:user_id) }
+
     end
 end
