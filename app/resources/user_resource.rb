5b511834ea95e33a85194882ac02c26cf7a15fe6
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:07:49 +0530

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,17 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :username, :string
+  attribute :avatar, :string
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :bio, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
