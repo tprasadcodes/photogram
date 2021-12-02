c818e0a8637dd785767563c038dc9fd76aa3eb4a
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:05:39 +0530

    Direct association between photos and comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :likes,
              :class_name => "Vote",
              :dependent => :destroy
