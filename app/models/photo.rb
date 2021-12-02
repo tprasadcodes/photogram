dde801ebd0b7332668f00ab48e7b8c91a117ac1f
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:05:23 +0530

    Direct association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,11 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :required => false,
+             :class_name => "User",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
