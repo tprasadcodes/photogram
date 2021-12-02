a064ed0bc2b006b8adceb98d441112e66c84865e
anonymous anonymous <reachtprasad@gmail.com>
2021-12-02 11:07:53 +0530

    Generate graphiti resources for photos

---
+++ app/resources/photo_resource.rb
@@ -0,0 +1,14 @@
+class PhotoResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :caption, :string
+  attribute :image, :string
+  attribute :location, :string
+  attribute :owner_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
---
+++ spec/api/v1/photos/create_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "photos#create", type: :request do
+  subject(:make_request) do
+    jsonapi_post "/api/v1/photos", payload
+  end
+
+  describe 'basic create' do
+    let(:params) do
+      attributes_for(:photo)
+    end
+    let(:payload) do
+      {
+        data: {
+          type: 'photos',
+          attributes: params
+        }
+      }
+    end
+
+    it 'works' do
+      expect(PhotoResource).to receive(:build).and_call_original
+      expect {
+        make_request
+        expect(response.status).to eq(201), response.body
+      }.to change { Photo.count }.by(1)
+    end
+  end
+end
